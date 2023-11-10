b27b03002fc33fdecbf44eec36b721a7bb9651f7
hjsupernova <kem08120506@gmail.com>
2023-11-10 12:14:54 +0900

    Project setup

---
+++ GitPractice2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitPractice2
+//
+//  Created by KHJ on 2023/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitPractice2/GitPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitPractice2App.swift
+//  GitPractice2
+//
+//  Created by KHJ on 2023/11/10.
+//
+
+import SwiftUI
+
+@main
+struct GitPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
